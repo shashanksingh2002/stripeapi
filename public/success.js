7cc09f3f2c09b50b3fc2683f5c45c1fe5a78634c
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2023-08-06 15:00:56 +0530

    Add files via upload

--- public/success.js
+++ public/success.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-const dataPlaceholder = document.querySelector('.transactiondata')
-const refund = document.querySelector('.refund');
-let id = null;
-document.addEventListener('DOMContentLoaded',() => {
-    fetch('/api/transaction',{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        id = data.id;
-        data.amount = Number(data.amount)/100;
-        const html = `
-            <p>Plan: ${data.plan}</p>
-            <p>Type: ${data.type}</p>
-            <p>Amount: ${data.amount}</p>
-        `;
-        dataPlaceholder.innerHTML = html;
-    })
-    .catch(err => console.error(err));
-});
-
-refund.addEventListener('click',() => {
-    const data = {
-        id:id
-    }
-    fetch('/api/refund',{
-        method:'DELETE',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(() => {
-        dataPlaceholder.innerHTML = `<p>Refund Initiated Successfully</p>`;
-    })
-    .catch(err => console.error(err));
-});
-
-
-=======
 const dataPlaceholder = document.querySelector('.transactiondata')
 const refund = document.querySelector('.refund');
 let id = null;
@@ -83,4 +39,3 @@ refund.addEventListener('click',() => {
 });
 
 
->>>>>>> 58affa397f2d73ae6f0e7255f1d9f373ec2a8a39
